1e6279a4ab4a3ee4b81e55b888a775992bd77a6a
zhouzhao <zhouzhao@163.com>
2020-09-24 10:16:02 +0800

    提交hello

--- src/main/java/com/javakc/test.java
+++ src/main/java/com/javakc/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void  main(String[] args) {
         System.out.println("123");
         System.out.println("ddd   提交ddd");
+        System.out.println("hello");
     }
 }
