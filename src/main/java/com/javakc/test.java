5ac64fe6743b6a70414abfb4da34b5bb62112099
zhouzhao <zhouzhao@163.com>
2020-09-24 10:32:23 +0800

    提交aaa

--- src/main/java/com/javakc/test.java
+++ src/main/java/com/javakc/test.java
@@ -3,7 +3,7 @@ package com.javakc;
 public class test {
 
     public static void  main(String[] args) {
-        System.out.println("123");
+        System.out.println("aaa");
         System.out.println("ddd   提交ddd");
         System.out.println("hello");
         System.out.println("word C");
