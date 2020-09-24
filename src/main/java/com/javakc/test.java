4cb35b8a2b3352504ae3443604ad764475794809
zhouzhao <zhouzhao@163.com>
2020-09-24 10:20:20 +0800

    提交word

--- src/main/java/com/javakc/test.java
+++ src/main/java/com/javakc/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("123");
         System.out.println("ddd   提交ddd");
         System.out.println("hello");
+        System.out.println("word");
     }
 }
